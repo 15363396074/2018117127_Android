db3af313cce713a381811eae392c5171564abc5d
unknown <1843174054@qq.com>
2021-06-12 12:47:03 +0800

    第三章

---
+++ ActivityText/app/src/main/java/com/example/activitytext/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.activitytext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static void actionStart(Context context, String data1, String data2) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        intent.putExtra("param1", data1);
+        intent.putExtra("param2", data2);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondActivity.this, "You clicked Button 2",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
